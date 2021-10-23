eac11123cc7feda275aa9746cba3b5f79dcd5b5a
oladolapo <66223952+oladolapo@users.noreply.github.com>
2021-10-23 12:46:15 +0100

    Add files via upload

---
+++ model/database.js
@@ -0,0 +1,23 @@
+exports.userDatabase = [{
+    id: 1,
+    name: "dolapo",
+    age: 23,
+    address: "ikeja",
+    photo: null
+}, {
+    id: 2,
+    name: "deji",
+    age: 26,
+    address: "magodo",
+    photo: null
+},
+{
+    id: 3,
+    name: "dotun",
+    age: 30,
+    address: "ogudu",
+    photo: null
+}
+];
+
+exports.contactDatabase = []
